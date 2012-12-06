fac006e5cdded731c52b76ac3dfe9fd398037a9e
Beraldo Leal <beraldo@beraldoleal.com>
2012-12-06 14:48:03 -0200

    Added some doc

--- gooserver/core/api/resources.py
+++ gooserver/core/api/resources.py
@@ -4,9 +4,20 @@ from django.db.models import Q
 from core.models import *
 
 class ApplicationResource(ModelResource):
+    """This resource handler app requests.
+
+    Allowed Methods:
+    ----------------
+
+        GET    /apps/                # Get all apps on grid
+        GET    /apps/{id}            # Get info about a app
+        GET    /apps/schema/         # Get app schema
+        GET    /apps/set/{id};{id}/  # Get a list of apps
+    """
     class Meta:
         queryset = Type.objects.all()
         resource_name = 'apps'
+        include_resource_uri = False
         fields = ['id', 'name', 'multi_hosts', 'multi_thread',]
         list_allowed_methods = ['get',]
         detail_allowed_methods = ['get',]
@@ -19,12 +30,27 @@ class ApplicationResource(ModelResource):
 #        return bundle
 
 class JobResource(ModelResource):
+    """This resource handler jobs requests.
+
+    Allowed Methods:
+    ----------------
+
+        GET    /jobs/                # Get all jobs of an user
+        GET    /jobs/{id}            # Get info about an user job
+        GET    /jobs/schema/         # Get job schema
+        GET    /jobs/set/{id};{id}/  # Get a list of jobs
+        POST   /jobs/                # Submmit a new job
+        PUT    /jobs/{id}/           # Update a job
+        PATCH  /jobs/{id}/           # Partially update a job
+        DELETE /jobs/{id}/           # Delete a job
+    """
     class Meta:
         # Remove from query deleted jobs
         queryset = Job.objects.filter(~Q(status = 'D'))
         resource_name = 'jobs'
-        list_allowed_methods = ['get',]
-        detail_allowed_methods = ['get',]
+        include_resource_uri = False
+        list_allowed_methods = ['get', 'post', ]
+        detail_allowed_methods = ['get', 'put', 'patch', 'delete', ]
         list_exclude = ['return_code', 'hosts', 'eta', 'input_obj_id',
                         'output_obj_id', 'memory_in_use', 'memory_requirement',
                         'start_time', 'restart', 'ttl', 'pph',
