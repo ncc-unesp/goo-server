0884c1508f82a868ca28ebcc74ed98430753d62b
Gabriel A. von Winckler <winckler@ncc.unesp.br>
2013-03-07 18:47:31 -0300

    JS: new .js dedicated to submit functions

--- portal/static/js/goo.js
+++ portal/static/js/goo.js
@@ -71,21 +71,6 @@ function render_login() {
             $('#login_box').mustache('login_template', {user: get_user()}, { method: 'html' });
         };
 
-function post_job(form) {
-    $.ajax({
-        dataType: "json",
-        type: "POST",
-        url: "/api/v1/jobs/?token=" + get_token(),
-        data: '{"app":"/api/v1/apps/33/", "args": "10", "executable": "/bin/sleep", "name":"job2" }',
-        //data: $(form).serializeArray(),
-        contentType: "application/json",
-        success: function(data) {
-            var resp = data;
-        }
-    })
-    return false;
-};
-
 function do_login() {
     current_user = get_user();
     if (current_user) return current_user;
