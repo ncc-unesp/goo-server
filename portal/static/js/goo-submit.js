06d4c89bb5b391b61d3c8f96fdb54a0be2b159d8
Gabriel A. von Winckler <winckler@ncc.unesp.br>
2013-03-11 18:08:46 -0300

    Portal file upload (2/2)

--- portal/static/js/goo-submit.js
+++ portal/static/js/goo-submit.js
@@ -1,39 +1,85 @@
-function create_job(form) {
-    find_dataproxy();
+function create_job() {
+    if ($('#inputFiles')[0].files.length)
+        find_dataproxy();
+    else
+        // no files
+        post_job([]);
     return false;
 }
 
 function find_dataproxy(){
     //find a dataproxy server and call upload_files
-
-    $.get("/api/v1/dataproxyserver/?token=" + get_token(),
-            function (data) {
-                addr = data["objects"][0].url;
-                upload_files(addr);
-            })
+    $.ajax({
+        type:"GET",
+        url: "/api/v1/dataproxyserver/?token=" + get_token(),
+        error: function (data) { 
+            return do_alert("Server error. (Request dataproxy failed)");
+        },
+        success: function (data) {
+            addr = data["objects"][0].url;
+            if(typeof addr == 'undefined')
+                return do_alert("Server error. (No data server found)");
+            upload_files(addr);
+        }
+    });
 }
 
 function upload_files(addr){
     //create a multipart-form, upload and call post_job
-    console.log(addr);
-
-    input_obj = "foo";
-    post_job(input_obj);
+    //var filesForm = new FormData($('inputFiles')[0]);
+    var filesForm = new FormData();
+    filesForm.append("files", $('#inputFiles')[0]);
+    filesForm.append("name", slugify($("#name")[0].value) + "-input.zip");
+    $.ajax({
+        type:"POST",
+        url: addr + "api/v1/dataproxy/objects/?compress&token=" + get_token(),
+        // Form data
+        data: filesForm,
+        dataType: "json",
+        //Options to tell JQuery not to process data or worry about content-type
+        cache: false,
+        contentType: false,
+        processData: false,
+        error: function (data) {
+            return do_alert("File upload error.");
+        },
+        success: function (data) {
+            input_obj = data["resource_uri"];
+            if(typeof input_obj == 'undefined')
+                return do_alert("Error on upload. (No object id found)");
+            post_job([input_obj]);
+        }
+    });
 }
 
-function post_job(form) {
-    //find dataproxy server
-    //upload files and save obj_url
-    //port job
+function post_job(input_objs) {
+    data = {};
+    form = $("#submit_form").serializeArray()
+    for (i in form)
+        data[form[i].name] = form[i].value;
+
+    data["input_objs"] = input_objs;
     $.ajax({
-        dataType: "json",
         type: "POST",
         url: "/api/v1/jobs/?token=" + get_token(),
-        data: '{"app":"/api/v1/apps/33/", "args": "10", "executable": "/bin/sleep", "name":"job2" }',
+        dataType: "json",
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify(data),
         //data: $(form).serializeArray(),
         contentType: "application/json",
+        error: function (data) { 
+            return do_alert("Error in the server. (Post job failed)");
+        },
         success: function(data) {
             var resp = data;
+            href("#jobs");
         }
-    })
-};
+    });
+}
+
+function slugify(text) {
+    text = text.replace(/[^-a-zA-Z0-9,&\s]+/ig, '');
+    text = text.replace(/-/gi, "_");
+    text = text.replace(/\s/gi, "-");
+    return text;
+}
