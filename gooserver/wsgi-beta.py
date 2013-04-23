d4ae86f5318cd06869a473fd8afbc3b9e064cf0d
Gabriel A. von Winckler <winckler@ncc.unesp.br>
2013-04-23 17:04:32 -0300

    Apache WSGI fix

--- gooserver/wsgi-beta.py
+++ gooserver/wsgi-beta.py
@@ -15,6 +15,9 @@ middleware here, or combine a Django application with an application of another
 framework.
 
 """
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gooserver.settings.beta")
+
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
