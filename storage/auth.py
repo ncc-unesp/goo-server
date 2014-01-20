166a0e280e1984da91d0fc449698e6ceb085c8af
Gabriel A. von Winckler <winckler@ncc.unesp.br>
2014-01-20 16:29:10 -0200

    Require additional server token in requests from DPS

--- storage/auth.py
+++ storage/auth.py
@@ -3,6 +3,8 @@
 from core.models import UserToken
 from dispatcher.models import Pilot
 
+from storage.models import DataProxyServer as DPS
+
 from django.utils.timezone import now
 
 from tastypie.authentication import Authentication
@@ -12,20 +14,30 @@ class AnyTokenAuthentication(Authentication):
     def is_authenticated(self, request, **kwargs):
         # check if token exists inside the request
         try:
-            token = request.REQUEST['token']
+            server_token = request.REQUEST['token']
+            user_token = request.REQUEST['user_token']
         except KeyError:
+            return False
+
+        # try to find server (DPS) id by token
+        try:
+            # query DB for servers whith this token
+            request.dps = DPS.objects.filter(enabled=True)
+            request.dps = request.dps.get(token=server_token)
+        except DPS.DoesNotExist, DPS.MultipleObjectsReturned:
+            # DPS token
             return False
 
         # lets try user tokens first
         try:
             # query DB for token and return user
             t = UserToken.objects.filter(expire_time__gt=now())
-            user = t.get(token=token).user
+            user = t.get(token=user_token).user
         except UserToken.DoesNotExist, UserToken.MultipleObjectsReturned:
             # now, lets try pilot tokens
             try:
                 # query DB for token and return pilot
-                pilot = Pilot.objects.filter(token=token).get()
+                pilot = Pilot.objects.filter(token=user_token).get()
 
                 job = pilot.get_current_job()
                 if not job:
