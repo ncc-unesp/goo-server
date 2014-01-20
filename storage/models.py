d46eac8951600eac8d827c21245b0b52c146c344
Gabriel A. von Winckler <winckler@ncc.unesp.br>
2014-01-20 16:25:57 -0200

    Fix class definition order

--- storage/models.py
+++ storage/models.py
@@ -1,6 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class DataProxyServer(models.Model):
+    name = models.CharField(max_length=255)
+    url = models.CharField(max_length=255)
+    enabled = models.BooleanField(default=True)
+    token = models.CharField(max_length=512)
+
+    def __repr__ (self):
+        return '<ObjectProxy %s>' % self
+
+    def __str__ (self):
+        return self.name
+
 class Object(models.Model):
     name = models.CharField(max_length=255)
     sha1 = models.CharField(max_length=40)
@@ -24,18 +36,6 @@ class Object(models.Model):
 
     def __repr__ (self):
         return '<Object %s>' % self
-
-    def __str__ (self):
-        return self.name
-
-class DataProxyServer(models.Model):
-    name = models.CharField(max_length=255)
-    url = models.CharField(max_length=255)
-    enabled = models.BooleanField(default=True)
-    token = models.CharField(max_length=512)
-
-    def __repr__ (self):
-        return '<ObjectProxy %s>' % self
 
     def __str__ (self):
         return self.name
