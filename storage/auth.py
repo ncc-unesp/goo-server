28fc6f8e4ed6d3ea0f51b865130e74a6ae822fed
Gabriel A. von Winckler <winckler@ncc.unesp.br>
2014-01-21 17:28:04 -0200

    Storage Auth fix

--- storage/auth.py
+++ storage/auth.py
@@ -15,7 +15,6 @@ class StorageAuthentication(Authentication):
     def is_authenticated(self, request, **kwargs):
         # check if token exists inside the request
         try:
-            server_token = request.REQUEST['proxy_token']
             user_token = request.REQUEST['token']
         except KeyError:
             return False
@@ -23,9 +22,10 @@ class StorageAuthentication(Authentication):
         # try to find server (DPS) id by token
         try:
             # query DB for servers whith this token
+            server_token = request.REQUEST['proxy_token']
             request.dps = DPS.objects.filter(enabled=True)
             request.dps = request.dps.get(token=server_token)
-        except DPS.DoesNotExist, DPS.MultipleObjectsReturned:
+        except (KeyError, DPS.DoesNotExist, DPS.MultipleObjectsReturned):
             # direct request, no DPS token
             request.dps = None
 
@@ -34,7 +34,7 @@ class StorageAuthentication(Authentication):
             # query DB for token and return user
             t = UserToken.objects.filter(expire_time__gt=now())
             user = t.get(token=user_token).user
-        except UserToken.DoesNotExist, UserToken.MultipleObjectsReturned:
+        except (UserToken.DoesNotExist, UserToken.MultipleObjectsReturned):
             # now, lets try pilot tokens
             try:
                 # query DB for token and return pilot
