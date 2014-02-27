a2dd29b8be9d6503484924d6886c666854b54c7f
Beraldo Leal <beraldo@beraldoleal.com>
2014-02-27 14:15:32 -0300

    added ask link to portal

--- core/api/resources.py
+++ core/api/resources.py
@@ -160,7 +160,7 @@ class JobResource(ModelResource):
             for item in list_exclude:
                 del bundle.data[item]
         else:
-            # detail request
+           # detail request
             bundle.data['slug'] = slugify(bundle.obj.name)
         bundle.data['app_name'] = bundle.obj.application
 
